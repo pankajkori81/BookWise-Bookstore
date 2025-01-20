2410161353e108149173b63673c6d6a5a10b1d47
Pankaj kori <93833439+pankajkori81@users.noreply.github.com>
2025-01-20 11:02:44 +0530

    Add files via upload

---
+++ frontend/src/bookcardAPI/bookapi.js
@@ -0,0 +1,90 @@
+import React from "react";
+
+
+const Bookcards=[
+    {   
+        _id:"1",
+        img:"https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1295750108i/8164754.jpg",
+        title:"Do or Die",
+        price:"1550rs"
+
+    },
+    {
+        
+        _id:"2",
+        img:"https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1295750108i/8164754.jpg",
+        title:"rich dada",
+        price:"2550rs"
+
+    }
+    ,{
+        
+        _id:"3",
+        img:"https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1295750108i/8164754.jpg",
+        title:"Win or loose",
+        price:"5550rs"
+
+    },
+    {
+
+        
+        _id:"4",
+        img:"https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1295750108i/8164754.jpg",
+        title:"Not Okay",
+        price:"1450rs"
+
+    }
+    ,
+    {
+
+        
+        _id:"5",
+        img:"https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1295750108i/8164754.jpg",
+        title:"Its Okay",
+        price:"1350rs"
+
+    }
+
+    ,
+    {
+
+        
+        _id:"6",
+        img:"https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1295750108i/8164754.jpg",
+        title:"oay ",
+        price:"1450rs"
+
+    }
+
+    ,
+    {
+
+        
+        _id:"7",
+        img:"https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1295750108i/8164754.jpg",
+        title:"Not Okay",
+        price:"1450rs"
+
+    },
+    {
+
+        
+        _id:"8",
+        img:"https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1295750108i/8164754.jpg",
+        title:"Not Okay",
+        price:"1450rs"
+
+    }
+    ,
+    {
+
+        
+        _id:"9",
+        img:"https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1295750108i/8164754.jpg",
+        title:"Not Okay",
+        price:"1450rs"
+
+    }
+]
+
+export default Bookcards;
